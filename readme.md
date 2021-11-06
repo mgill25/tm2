9a3afd397ca228bf6172bb61f6fd7dbfecd09a67
Manish Gill <gill.manish90@gmail.com>
2021-11-06 01:32:36 +0100

    Initial commit - Help printing works

---
+++ readme.md
@@ -0,0 +1,6 @@
+# TM - The alacritty Theme Changer
+
+I change themes frequently during my Terminal sessions in alacritty.
+For base16 themes, there is also a lot of overlap between themes used by the terminal as well as themes used by vim.
+
+This tool is just a small little wrapper that will allow theme changes.
---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use std::env;
+
+#[macro_use]
+extern crate lazy_static;
+
+/// Parses the binary absolute path and returns the binary name
+fn parse_bin_name(bin_path: &str) -> &str {
+    bin_path.split("/").last().unwrap()
+}
+
+#[derive(Clone)]
+struct Flag {
+    name: String,
+    desc: String,
+}
+
+// Define all the flags
+lazy_static! {
+    // we wanted a "global", without having a global.
+    // since "globals are bad", rust makes it tricky.
+    // Question 1: Why does `const` not work here?
+    // Question 2: What is the difference between a const and a static?
+    // Question 3: Why does a vanilla static not work here without the lazy_static crate?
+    static ref FLAGS: Vec<Flag> = vec![
+        Flag {
+            name: "help".to_string(),
+            desc: "Prints the help string".to_string(),
+        },
+        Flag {
+            name: "with-vim".to_string(),
+            desc: "Also tries to set the same colorscheme in vim".to_string(),
+        },
+    ];
+}
+
+/// Generate the usage string for the tool
+fn gen_usage(bin_name: &str) -> String {
+    let topline = format!("{} <theme> [options]\n", bin_name);
+
+    let mut secondline = format!("\nOPTIONS:\n");
+    for flag in FLAGS.to_vec() {
+        secondline.push_str(format!("\t--{}: \t {}\n", flag.name, flag.desc).as_str());
+    }
+
+    format!("{}{}", topline, secondline)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let bin_name = parse_bin_name(&args[0]);
+    if args.len() == 1 || (&args[1] == "--help" || &args[1] == "-h") {
+        let usage = gen_usage(&bin_name);
+        println!("Usage:\n\t{}", usage);
+    } else {
+    }
+}
