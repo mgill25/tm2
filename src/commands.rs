65ce05b2a75dd72bd11f98d70df7a007950e4e5c
Manish Gill <gill.manish90@gmail.com>
2021-11-19 00:25:48 +0100

    Implementing --list

--- src/commands.rs
+++ src/commands.rs
@@ -5,20 +5,23 @@ use crate::s;
 use crate::FLAGS;
 use log::{debug, info};
 
+const DEFAULT_THEME_ROOT: &str = "/Users/gill/.config/alacritty/alacritty-theme/themes";
+
 /// Global handler function for all Instructions
 /// Invokes unique handlers for each specific command/option we get from the Instruction
 pub fn handle(ins: Instruction) {
     debug!("HANDLE INS = {:?}", ins);
+    let handler = Handler {};
     match ins {
         Instruction { command, option } => {
             debug!("\tCMD = {:?}", command);
             debug!("\tOPT = {:?}", option);
             match command {
-                Some(Command::Help(bin_name)) => cmd_print_help(bin_name),
-                Some(Command::ListThemes) => cmd_list_themes(),
+                Some(Command::Help(bin_name)) => handler.print_help(&bin_name),
+                Some(Command::ListThemes) => handler.list_themes(),
                 Some(Command::SwitchTheme(new_theme)) => match option {
-                    Some(CommandOption::SwitchWithVim) => cmd_handle_switch_with_vim(&new_theme),
-                    _ => cmd_handle_switch(&new_theme),
+                    Some(CommandOption::SwitchWithVim) => handler.switch_with_vim(&new_theme),
+                    _ => handler.switch(&new_theme),
                 },
                 _ => {}
             }
@@ -26,25 +29,44 @@ pub fn handle(ins: Instruction) {
     }
 }
 
-pub fn cmd_print_help(bin_name: String) {
-    let topline = format!("{} [options] <parameter>\n", bin_name);
-    let mut secondline = s("\nOPTIONS:\n");
-    for flag in FLAGS.to_vec() {
-        secondline.push_str(format!("\t{}: \t {}\n", flag.name, flag.desc).as_str());
+struct Handler {}
+
+impl Handler {
+    pub fn print_help(&self, bin_name: &String) {
+        let topline = format!("{} [options] <parameter>\n", bin_name);
+        let mut secondline = s("\nOPTIONS:\n");
+        for flag in FLAGS.to_vec() {
+            secondline.push_str(format!("\t{}: \t {}\n", flag.name, flag.desc).as_str());
+        }
+        let usage = format!("{}{}", topline, secondline);
+        println!("Usage:\n\t{}", usage);
     }
-    let usage = format!("{}{}", topline, secondline);
-    println!("Usage:\n\t{}", usage);
-}
 
-fn cmd_list_themes() {
-    info!("This should list all the available themes!");
-}
+    /// List the themes in the root theme directory
+    fn list_themes(&self) {
+        use std::fs;
+        // info!("This should list all the available themes!");
+        // How do I list files in a directory using Rust?
+        // exa -lah ~/.config/alacritty/alacritty-theme/themes
+        let themes_filepath = DEFAULT_THEME_ROOT;
+        let themes_dir = fs::read_dir(themes_filepath).unwrap();
 
-fn cmd_handle_switch(new_theme: &String) {
-    info!("This should switch the terminal theme to {}", new_theme);
-}
+        for theme in themes_dir {
+            let theme_fname = theme.unwrap().file_name();
+            let theme_fstr = theme_fname.to_str().unwrap();
 
-fn cmd_handle_switch_with_vim(new_theme: &String) {
-    cmd_handle_switch(&new_theme);
-    info!("This will also try to change the same theme in neovim");
+            // split the string and remove the extension
+            let theme_name = theme_fstr.split(".").collect::<Vec<&str>>();
+            println!("{}", &theme_name[0]);
+        }
+    }
+
+    fn switch(&self, new_theme: &String) {
+        info!("This should switch the terminal theme to {}", new_theme);
+    }
+
+    fn switch_with_vim(&self, new_theme: &String) {
+        self.switch(&new_theme);
+        info!("This will also try to change the same theme in neovim");
+    }
 }
